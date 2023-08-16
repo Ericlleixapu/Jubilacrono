93ebd09e76ae14501295a7e5d62f99db432fd265
unknown <ericlleixa@outlook.com>
2023-08-16 02:29:15 +0200

    Signed-off-by: unknown <ericlleixa@outlook.com>

---
+++ Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿
+namespace Jubilacrono
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timetoend = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.SuspendLayout();
+            // 
+            // timetoend
+            // 
+            this.timetoend.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.timetoend.AutoSize = true;
+            this.timetoend.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timetoend.Location = new System.Drawing.Point(20, 9);
+            this.timetoend.Name = "timetoend";
+            this.timetoend.Size = new System.Drawing.Size(143, 62);
+            this.timetoend.TabIndex = 1;
+            this.timetoend.Text = "1200 Dias \r\n00:00:00";
+            this.timetoend.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker1.Location = new System.Drawing.Point(41, 84);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(96, 20);
+            this.dateTimePicker1.TabIndex = 4;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(182, 113);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.timetoend);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Jubilacron";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label timetoend;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Resources;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Jubilacrono
+{
+    public partial class Form1 : Form
+    {
+        DateTime limite = new DateTime(2026, 11, 30, 13, 45, 00);
+        DateTime currentDateTime;
+        TimeSpan diferencia;
+
+        public Form1()
+        {
+            InitializeComponent();
+            try
+            {
+                IResourceReader rr = new ResourceReader("myResources.resources");
+                foreach (System.Collections.DictionaryEntry d in rr)
+                {
+                    limite = (DateTime)d.Value;
+                }
+                //Close the resxReader
+                rr.Close();
+            }catch(Exception e)
+            {
+                Console.WriteLine("no hay elementos guardados.");
+            }
+
+            dateTimePicker1.Value = limite;
+
+            currentDateTime = DateTime.Now;
+            diferencia = limite - currentDateTime;
+            timetoend.Text = diferencia.Days + " Dias \n" + diferencia.Hours + ":" + diferencia.Minutes + ":" + diferencia.Seconds;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            currentDateTime = DateTime.Now;
+            diferencia = limite - currentDateTime;
+            timetoend.Text = diferencia.Days + " Dias \n" +diferencia.Hours +":" + diferencia.Minutes +":"+ diferencia.Seconds;
+        }
+        private void monthCalendar1_DateChanged(object sender, DateRangeEventArgs e)
+        {
+
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            limite = dateTimePicker1.Value;
+            IResourceWriter rw = new ResourceWriter("myResources.resources");
+            rw.AddResource("limite", limite);
+            rw.Close();
+        }
+    }
+}
